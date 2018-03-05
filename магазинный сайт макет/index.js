70d47ee77ef4a2d2152d9d2882bfa1c79056cbe0
4t5ghy5 <36933956+4t5ghy5@users.noreply.github.com>
2018-03-05 15:26:05 +0100

    Add files via upload

---
+++ магазинный сайт макет/index.js
@@ -0,0 +1,20 @@
+window.onload=init;
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+function init(){
+document.getElementById("defaultOpen").click();
+}
